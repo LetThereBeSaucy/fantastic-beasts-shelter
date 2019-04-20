d87317c05001876d864f0c71c99b94488ea1c5e9
LetThereBeSaucy <jeff9878890@gmail.com>
2019-04-20 14:21:59 -0600

    Animal model setup

---
+++ app/controllers/animals_controller.rb
@@ -0,0 +1,24 @@
+class AnimalsController < ApplicationController
+  def index
+    @animals = Animal.all
+  end
+
+  def new
+  end
+
+  def create
+    @animal = Animal.new(params.require(:animal).permit(:name))
+
+    @animal.save
+    redirect_to @animal
+  end
+
+  def show
+    @animal = Animal.find(params[:id])
+  end
+
+  private
+    def animals_params
+      params.require(:animal).permit(:name)
+    end
+end
